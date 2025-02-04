874d4abd14b03589621c17faf450af418c5a652b
Gabriel Souza <87774582+gabriel9966@users.noreply.github.com>
2025-02-04 17:56:24 -0300

    aulas 26,27,28,29

---
+++ aula27(concatenacao).py
@@ -0,0 +1,5 @@
+nome = 'Gabriel' + ' ' + 'Souza'# concatenacao :Concatenação é o processo de combinar ou unir duas ou mais strings (sequências de caracteres) em uma única string. 
+print(nome)
+
+a = 'a' * 10  #repeticao
+print(a)
